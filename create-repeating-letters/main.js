2bf4eac660e90cc4a3e3ede2a578b681e88c2036
Connor Ashlock <connor9804@yahoo.com>
2020-07-31 19:15:48 -0700

    Changes to create-repeating-letters

--- create-repeating-letters/main.js
+++ create-repeating-letters/main.js
@@ -1,8 +1,6 @@
 function createRepeatingLetters(word, numOfRepeats){
   var repeatLetter = '';
   for (var i = 0; i < word.length; i++){
-    for (var j = 0; j < word.length; j++) {
-      repeatLetter += word.charAt(j).repeat(numOfRepeats);
+      repeatLetter += word.charAt(i).repeat(numOfRepeats);
     } return repeatLetter
-  }
 }
