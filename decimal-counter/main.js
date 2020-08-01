f91f3681db4497a18f810e4ab8f7e420006d3733
Connor Ashlock <connor9804@yahoo.com>
2020-08-01 11:43:33 -0700

    Completed decimal counter

--- decimal-counter/main.js
+++ decimal-counter/main.js
@@ -1,5 +1,9 @@
 function decimalPlaceCounter(stringNumber){
-  var result = '.';
-  for (var i = 0; i < stringNumber.length; i++)
-    stringNumber.indexOf('.')
+  var result = 0;
+  for (var i = 0; i < stringNumber.length; i++){
+    if (stringNumber[i] === "."){
+      result = stringNumber.length - i - 1;
+    }
+  }
+  return result;
 }
